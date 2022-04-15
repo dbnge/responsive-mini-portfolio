e3a82f6e6b14945ad72996fcabf7bfc4f7a7245e
dbunge <dominik.bnge@gmail.com>
2022-04-15 19:42:09 +0200

    Add files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+/* ==================== DISPLAY CONTACT FORM ==================== */
+
+const contactButton = document.querySelector(".profile__contact .profile__contact-hello .hello");
+const closeButton = document.querySelector(".form .form__close .close__link");
+const overlay = document.querySelector(".overlay");
+const contactForm = document.querySelector(".profile__form");
+
+// Contact Form opens and overlay is applied if user clicks on "Say Hello" button
+contactButton.addEventListener("click", () => {
+    contactForm.classList.remove("hide");
+    overlay.classList.add("show");
+})
+
+// Contact Form closes if user clicks on the close button
+closeButton.addEventListener("click", () => {
+    contactForm.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+// Contact Form closes if user clicks anywhere on the overlay 
+overlay.addEventListener("click", () => {
+    contactForm.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+
 /* ==================== VISIBILITY PROJECTS/SKILLS TAB ==================== */
 
 const tabs = document.querySelectorAll('[data-target]'),
@@ -35,5 +61,7 @@ sr.reveal(`.profile__social`, {delay: 500})
 sr.reveal(`.profile__info-group`, {interval: 100, delay: 600})
 sr.reveal(`.profile__contact`, {delay: 700})
 sr.reveal(`.filter`, {delay: 800})
+
+
 
 
